3d7f127600fcdd4e3d73a9f3891904925833f4a7
Dilshan Wijerathna <dtdwijerathna@students.nsbm.ac.lk>
2025-12-24 15:51:57 +0530

    Add offers and trending destinations to home page, and implement styled footer component

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import {MainHeader} from './inner-item/main-header/main-header';
 import {OptionsComponent} from './inner-item/options-component/options-component';
 import {FooterComponent} from '../../components/footer-component/footer-component';
+import {OffersComponent} from './inner-item/offers-component/offers-component';
+import {HomeTrendingDestinations} from './inner-item/home-trending-destinations/home-trending-destinations';
 
 @Component({
   selector: 'app-home-page',
   imports: [
     MainHeader,
     OptionsComponent,
+    OffersComponent,
+    HomeTrendingDestinations,
 
   ],
   templateUrl: './home-page.html',
